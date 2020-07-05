18f5bd8e43196c972b826c8ba736fbe6388edd5e
hieuthanhpy99 <hieuthanhpy99@gmail.com>
2020-07-05 12:17:00 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Tech Blogs')
+    .setDescription('The Tech Blogs APIs description')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000, () => {
+    console.log('Listening at http://localhost:3000');
+    console.log('Swagger is available at http://localhost:3000/api-docs');
+  });
+}
+bootstrap();
